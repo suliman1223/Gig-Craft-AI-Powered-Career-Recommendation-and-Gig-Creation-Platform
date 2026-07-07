c1f00d9573e2e81751dc37ab9aa3fc1ec59aa7c3
Sulaiman Khan <sk2537987@gmail.com>
2026-07-07 23:54:27 +0500

    Added backend logic of all logins data.

--- backend/server/routes/authRoutes.js
+++ backend/server/routes/authRoutes.js
@@ -1,11 +1,15 @@
 const express=require("express");
-const { Signup, login } =require("../controllers/authController.js");
-const { protect } =require("../middleware/authMiddleware.js");
+const { Signup, login,getMe,logout,googleLogin,forgotPassword, resetPassword  } =require("../controllers/authController.js");
+const protect = require("../middleware/authMiddleware.js");
 
 const router = express.Router();
 
 router.post("/signup", Signup);
+router.post("/google", googleLogin);
 router.post("/login", login);
-
+router.get("/me", protect, getMe);
+router.post("/logout",  logout);
+router.post("/forgot-password", forgotPassword);
+router.put("/reset-password/:token", resetPassword);
 
 module.exports = router;
